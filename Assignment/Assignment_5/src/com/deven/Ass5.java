25c9cf6b1c82871d8668bcc7fcb14e1b680d049c
D5-96525-deven <heydven@gmail.com>
2026-04-20 19:00:16 +0530

    Assignment_5

---
+++ Assignment/Assignment_5/src/com/deven/Ass5.java
@@ -0,0 +1,42 @@
+package com.deven;
+
+public class Ass4 
+{
+
+	public Ass4() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main1 (String[] args)
+		{
+			String s = "Deven";
+			String newS = new StringBuilder(s).reverse().toString();
+			System.out.println("Old String-> "+s+"\n New String-> "+newS);
+		}
+	public static void main2 (String[] args)
+		{
+				String s = "Civic";
+				StringBuilder newS = new StringBuilder(s);
+				newS.reverse()
+;				if (s.equalsIgnoreCase(newS.toString())) 
+				{
+    				System.out.println("pali");
+				} else 
+				{
+				    System.out.println("Not pali");
+				}
+			
+		}
+	public static void main (String[] args)
+		{
+		String str = "apple banana cherry";
+		String[] fruits = str.split(" "); 
+		System.out.println(fruits.length);
+		}
+
+
+	
+
+
+}
